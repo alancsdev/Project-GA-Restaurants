606a1593230c391cc3958b826994beccf7c04b0b
Alan Santos <alanchs@live.com>
2024-03-06 18:13:03 +0800

    fixed form redirecting to wrong page

--- public/javascripts/order.js
+++ public/javascripts/order.js
@@ -108,6 +108,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         event.preventDefault();
       } else {
+        event.preventDefault();
         fetch('/order/add', {
           method: 'POST',
           headers: {
